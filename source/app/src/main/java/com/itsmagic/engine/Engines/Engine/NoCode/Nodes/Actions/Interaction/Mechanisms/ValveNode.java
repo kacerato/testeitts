89f376b29beeb62c12fda2688aadc8bbf30bf5c4
kacerato <79872194+kacerato@users.noreply.github.com>
2026-08-17 01:29:49 -0300

    fix(nocode): unify valve analog value with interaction registry

--- source/app/src/main/java/com/itsmagic/engine/Engines/Engine/NoCode/Nodes/Actions/Interaction/Mechanisms/ValveNode.java
+++ source/app/src/main/java/com/itsmagic/engine/Engines/Engine/NoCode/Nodes/Actions/Interaction/Mechanisms/ValveNode.java
@@ -3,6 +3,7 @@ package com.itsmagic.engine.Engines.Engine.NoCode.Nodes.Actions.Interaction.Mech
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.itsmagic.engine.Engines.Engine.NoCode.Interaction.InteractionRegistry;
+import com.itsmagic.engine.Engines.Engine.NoCode.Interaction.Runtime.InteractionDispatcher;
 import com.itsmagic.engine.Engines.Engine.NoCode.NoCodeData;
 import com.itsmagic.engine.Engines.Engine.NoCode.NoCodeNode;
 import com.itsmagic.engine.Engines.Engine.NoCode.NoCodeSlot;
@@ -16,62 +17,30 @@ import ga.o;
 import ga.p;
 import gb.C13317e;
 
-/**
- * Controlador de interacao analogica continua (Valvula, Manivela, Registro, Slider)
- * variando de 0.0 a 1.0.
- */
+/** Controlador analogico continuo para valvula/manivela/registro/slider. */
 public class ValveNode extends NoCodeNode implements F {
 
     public static final String SERIALIZED_NAME = "Interaction.Valve";
-
     public final NoCodeSlot[] inputs;
     public final NoCodeSlot[] outputs;
 
     public static class NodeFactory implements p {
-        @Override
-        public NoCodeNode a() {
-            return new ValveNode();
-        }
-
-        @Override
-        public Class<? extends NoCodeNode> b() {
-            return ValveNode.class;
-        }
-
-        @Override
-        public String c() {
-            return SERIALIZED_NAME;
-        }
-
-        @Override
-        public NoCodeNode d(JsonObject json) {
-            return (NoCodeNode) X7.a.m().fromJson((JsonElement) json, ValveNode.class);
-        }
-
-        @Override
-        public String e() {
-            return "Actions/Mechanisms";
-        }
-
-        @Override
-        public String f() {
-            return "Analog Valve Controller";
-        }
-
-        @Override
-        public boolean g() {
-            return true;
-        }
+        public NoCodeNode a() { return new ValveNode(); }
+        public Class<? extends NoCodeNode> b() { return ValveNode.class; }
+        public String c() { return SERIALIZED_NAME; }
+        public NoCodeNode d(JsonObject json) { return (NoCodeNode) X7.a.m().fromJson((JsonElement) json, ValveNode.class); }
+        public String e() { return "Actions/Mechanisms"; }
+        public String f() { return "Analog Valve Controller"; }
+        public boolean g() { return true; }
     }
 
-    static {
-        o.a(new NodeFactory());
-    }
+    static { o.a(new NodeFactory()); }
 
     public ValveNode() {
         this.inputs = new NoCodeSlot[]{
             new NoCodeSlot("Valve Object", H.GAME_OBJECT).c("Valve Object"),
-            new NoCodeSlot("Add Delta", H.NUMBER).c("Add Delta")
+            new NoCodeSlot("Value / Delta", H.NUMBER).c("Value / Delta"),
+            new NoCodeSlot("Mode", H.TEXT).c("Mode")
         };
         this.outputs = new NoCodeSlot[]{
             new NoCodeSlot("Changed", H.BRANCH).c("Changed"),
@@ -82,71 +51,36 @@ public class ValveNode extends NoCodeNode implements F {
         this.serializedNodeType = SERIALIZED_NAME;
     }
 
-    @Override
-    public NoCodeSlot[] F() {
-        return this.inputs;
-    }
+    public NoCodeSlot[] F() { return inputs; }
+    public NoCodeSlot[] J() { return outputs; }
 
-    @Override
-    public NoCodeSlot[] J() {
-        return this.outputs;
-    }
-
-    @Override
     public void m0() {
-        GameObject valve = Aa.b.b(this, this.f79021a, this.inputs[0]);
-        if (!C13317e.J(valve) && this.f79021a != null) {
-            valve = this.f79021a.h0();
-        }
+        GameObject valve = Aa.b.b(this, this.f79021a, inputs[0]);
+        if (!C13317e.J(valve) && this.f79021a != null) valve = this.f79021a.h0();
+        if (!C13317e.J(valve)) { u(outputs[0]); return; }
 
-        if (!C13317e.J(valve)) {
-            u(this.outputs[0]);
-            return;
-        }
+        float input = m.V(Q(inputs[1]));
+        String mode = m.Y(Q(inputs[2]));
+        float next = "Set".equalsIgnoreCase(mode) ? input : InteractionRegistry.getAnalogValue(valve) + input;
+        InteractionRegistry.setAnalogValue(valve, next);
+        float value = InteractionRegistry.getAnalogValue(valve);
+        InteractionRegistry.setAttribute(valve, "analog_value", Float.valueOf(value));
+        InteractionDispatcher.dispatchCustomEvent("interaction_value_changed", valve, Float.valueOf(value));
 
-        float delta = m.V(Q(this.inputs[1]));
-        Object currentValObj = InteractionRegistry.getAttribute(valve, "analog_value");
-        float currentVal = currentValObj instanceof Number ? ((Number) currentValObj).floatValue() : 0.0f;
-
-        float newVal = Math.max(0.0f, Math.min(1.0f, currentVal + delta));
-        InteractionRegistry.setAttribute(valve, "analog_value", Float.valueOf(newVal));
-
-        y0(this.outputs[3], Float.valueOf(newVal));
-
-        if (newVal <= 0.0f) {
-            u(this.outputs[1]);
-        } else if (newVal >= 1.0f) {
-            u(this.outputs[2]);
-        } else {
-            u(this.outputs[0]);
-        }
+        y0(outputs[3], Float.valueOf(value));
+        if (value <= 0.0f) u(outputs[1]);
+        else if (value >= 1.0f) u(outputs[2]);
+        else u(outputs[0]);
     }
 
-    @Override
-    public EnumC13304B M() {
-        return EnumC13304B.BOTH;
-    }
-
-    @Override
-    public String N(NoCodeData graphData) {
-        return "Analog Valve Controller";
-    }
-
-    @Override
+    public EnumC13304B M() { return EnumC13304B.BOTH; }
+    public String N(NoCodeData graphData) { return "Analog Valve Controller"; }
+    public String x(NoCodeData graphData) { return "Analog Valve Controller"; }
     public String a(int inputIndex, H desiredType) {
         if (inputIndex == 0) return "owner";
         if (inputIndex == 1) return "0.1";
+        if (inputIndex == 2) return "Add";
         return "";
     }
-
-    @Override
-    public H t0(int index, D resolver) {
-        if (index == 3) return H.NUMBER01;
-        return H.BRANCH;
-    }
-
-    @Override
-    public String x(NoCodeData graphData) {
-        return "Analog Valve Controller";
-    }
+    public H t0(int index, D resolver) { return index == 3 ? H.NUMBER01 : H.BRANCH; }
 }
